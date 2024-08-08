9a95f8f1ad40436c79a688e1e49c6b0bb8ffff0b
Matheus <117780916+conradocmatheus@users.noreply.github.com>
2024-08-08 11:30:52 -0300

    Book service additions

--- src/main/java/app/library/service/BookService.java
+++ src/main/java/app/library/service/BookService.java
@@ -1,4 +1,34 @@
 package app.library.service;
 
+import app.library.entity.Book;
+import app.library.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+
+    public String save(Book book){
+        return "Book successfully saved!";
+    }
+
+    public Book findById(Long id){
+
+        if (id == 1) {
+            Book book = new Book();
+            book.setName("IT");
+            book.setAuthor("Stephen King");
+            book.setEditor("Graciota");
+            book.setYear(1995);
+            return book;
+        }
+        else {
+            return null;
+        }
+    }
 }
