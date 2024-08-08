f73c962633510bda7044c01addb0238ef161af4a
Matheus <117780916+conradocmatheus@users.noreply.github.com>
2024-08-08 15:25:40 -0300

    Added @GeneratedValue to id at book.java

--- src/main/java/app/library/entity/Book.java
+++ src/main/java/app/library/entity/Book.java
@@ -1,12 +1,15 @@
 package app.library.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Book {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String author;
