9e1551244b7f66e4d9e03c19487e21f072d6e724
Matheus <117780916+conradocmatheus@users.noreply.github.com>
2024-08-08 10:32:01 -0300

    Initial commit

---
+++ src/main/java/app/library/entity/Book.java
@@ -0,0 +1,44 @@
+package app.library.entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Book {
+
+    private String name;
+    private String author;
+    private String editor;
+    private int year;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getEditor() {
+        return editor;
+    }
+
+    public void setEditor(String editor) {
+        this.editor = editor;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
