51976b27fd3df239300d6853a4b09b03c3dd318c
conradocmatheus <conrado.matheus@protonmail.com>
2024-08-14 21:06:40 -0300

    Pieces types are now one class

--- src/main/java/app/library/entity/PieceType.java
+++ src/main/java/app/library/entity/PieceType.java
@@ -14,7 +14,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Book {
+public class PieceType {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
