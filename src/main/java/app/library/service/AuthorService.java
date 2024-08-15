dbe7cb62dfa3ec4d6c29a54bf266fc486d1d51a8
conradocmatheus <conrado.matheus@protonmail.com>
2024-08-15 14:55:24 -0300

    AuthorService basic methods

--- src/main/java/app/library/service/AuthorService.java
+++ src/main/java/app/library/service/AuthorService.java
@@ -1,4 +1,44 @@
 package app.library.service;
 
+import app.library.entity.Author;
+import app.library.repository.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class AuthorService {
+
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    // Save an Author
+    public String save(Author author){
+        this.authorRepository.save(author);
+        return "Author: " + author.getName() + ", successfully saved";
+    }
+
+    // Update an Author by ID
+    public String update(Author author, Long id){
+        author.setId(id);
+        authorRepository.save(author);
+        return (author.getName() + " successfully updated!");
+    }
+
+    // Delete an Author by ID
+    public String delete(Long id){
+        this.authorRepository.deleteById(id);
+        return "Car with id: " + id + "deleted";
+    }
+
+    // List all Authors
+    public List<Author> listAll(){
+        return this.authorRepository.findAll();
+    }
+
+    // Find an Author by ID
+    public Author findById(Long id){
+        return this.authorRepository.findById(id).get();
+    }
 }
