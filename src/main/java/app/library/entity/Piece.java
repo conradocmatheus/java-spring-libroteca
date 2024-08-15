6e7b70777f571df979c902e555a99184a9d78dfd
conradocmatheus <conrado.matheus@protonmail.com>
2024-08-15 13:49:45 -0300

    Piece.java validations annotations added

--- src/main/java/app/library/entity/Piece.java
+++ src/main/java/app/library/entity/Piece.java
@@ -2,8 +2,7 @@ package app.library.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,34 +20,41 @@ public class Piece {
     private Long id;
 
     @NotBlank
+    @Size(min = 2, max = 50)
     private String name;
 
+    @NotNull
     @JsonIgnoreProperties("pieces")
     @ManyToOne(cascade = CascadeType.PERSIST)
     private PieceType pieceType;
 
-    @NotBlank
+    @NotNull
     @JsonIgnoreProperties("pieces")
     @ManyToMany(cascade = CascadeType.PERSIST)
     private Author author;
 
-    @NotBlank
+    @NotNull
     @JsonIgnoreProperties("pieces")
     @ManyToOne(cascade = CascadeType.PERSIST)
     private Publisher publisher;
 
-    @NotBlank
+    @NotNull
     @JsonIgnoreProperties("pieces")
     @ManyToOne(cascade = CascadeType.PERSIST)
     private Genre genre;
 
+    @NotNull
     @JsonIgnoreProperties("pieces")
     @ManyToOne(cascade = CascadeType.PERSIST)
     private Sale sale;
 
+    @Min(0)
+    @Positive
     private Integer year;
 
+    @Positive
     private Double value;
 
+    @Min(0)
     private Integer pages;
 }
