f300341d31a6e3e2fb532bd705ee6450dba2cc78
conradocmatheus <conrado.matheus@protonmail.com>
2024-08-15 15:21:31 -0300

    EmployeeService basic methods

---
+++ src/main/java/app/library/service/EmployeeService.java
@@ -0,0 +1,44 @@
+package app.library.service;
+
+import app.library.entity.Employee;
+import app.library.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    // Save an Employee
+    public String save(Employee employee) {
+        employeeRepository.save(employee);
+        return "Employee: " + employee.getName() + ", successfully saved";
+    }
+
+    // Update an Employee by ID
+    public String update(Employee employee, Long id) {
+        employee.setId(id);
+        employeeRepository.save(employee);
+        return employee.getName() + " successfully updated!";
+    }
+
+    // Delete an Employee by ID
+    public String delete(Long id) {
+        employeeRepository.deleteById(id);
+        return "Employee with id: " + id + " deleted";
+    }
+
+    // List all Employees
+    public List<Employee> listAll() {
+        return employeeRepository.findAll();
+    }
+
+    // Find an Employee by ID
+    public Employee findById(Long id) {
+        return employeeRepository.findById(id).get();
+    }
+}
