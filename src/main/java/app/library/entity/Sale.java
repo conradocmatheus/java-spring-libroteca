cfb2d7c5fb06b3b68379a73564a79e1dc9b41987
conradocmatheus <conrado.matheus@protonmail.com>
2024-08-15 14:00:08 -0300

    Sale.java validations annotations added

--- src/main/java/app/library/entity/Sale.java
+++ src/main/java/app/library/entity/Sale.java
@@ -3,11 +3,14 @@ package app.library.entity;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Entity
@@ -21,18 +24,27 @@ public class Sale {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
     @JsonIgnoreProperties("sales")
     @ManyToOne(cascade = CascadeType.PERSIST)
     private Employee employee;
 
+    @NotNull
     @JsonIgnoreProperties("sale")
     @OneToOne(cascade = CascadeType.PERSIST)
     private Client client;
 
+    @NotNull
     @JsonIgnoreProperties("sale")
     @OneToMany(mappedBy = "sale")
     private List<Piece> pieces;
 
     @NotNull
+    @Positive
     private Double totalValue;
+
+    @DateTimeFormat
+    private LocalDate saleDate;
+
+    private String paymentMethod;
 }
