3503087e015938ad91a354a8e322bf96842699e6
conradocmatheus <conrado.matheus@protonmail.com>
2024-08-15 15:17:31 -0300

    ClientService basic methods

--- src/main/java/app/library/service/ClientService.java
+++ src/main/java/app/library/service/ClientService.java
@@ -1,4 +1,44 @@
 package app.library.service;
 
+import app.library.entity.Client;
+import app.library.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ClientService {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    // Save a Client
+    public String save(Client client) {
+        clientRepository.saveAndFlush(client); // Use saveAndFlush for persistence
+        return "Client: " + client.getName() + ", successfully saved";
+    }
+
+    // Update a Client by ID
+    public String update(Client client, Long id) {
+        client.setId(id);
+        clientRepository.saveAndFlush(client); // Use saveAndFlush for persistence
+        return client.getName() + " successfully updated!";
+    }
+
+    // Delete a Client by ID
+    public String delete(Long id) {
+        clientRepository.deleteById(id);
+        return "Client with id: " + id + " deleted";
+    }
+
+    // List all Clients
+    public List<Client> listAll() {
+        return clientRepository.findAll();
+    }
+
+    // Find a Client by ID
+    public Client findById(Long id) {
+        return clientRepository.findById(id).get();
+    }
 }
