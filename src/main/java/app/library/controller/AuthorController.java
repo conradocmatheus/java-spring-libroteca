bc6326b8c564132fe89867621888a55156255f08
conradocmatheus <conrado.matheus@protonmail.com>
2024-08-16 10:41:57 -0300

    First save method for testing in authorController

---
+++ src/main/java/app/library/controller/AuthorController.java
@@ -0,0 +1,30 @@
+package app.library.controller;
+
+
+import app.library.entity.Author;
+import app.library.service.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/author")
+public class AuthorController {
+
+    @Autowired
+    private AuthorService authorService;
+
+    @PostMapping("/save")
+    public ResponseEntity<String> save(@RequestBody Author author){
+        try {
+            String msg = this.authorService.save(author);
+            return new ResponseEntity<>(msg, HttpStatus.CREATED);
+        } catch (Exception e){
+            return new ResponseEntity<>("Error: " + e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+}
